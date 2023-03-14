72eecca5f3427a833563e5cb96d1c9a7a75f90a1
piyushgautam258 <118935811+piyushgautam258@users.noreply.github.com>
2023-03-14 15:37:51 +0530

    Home

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
-import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route,Link, useNavigate} from "react-router-dom";
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
+import { useState } from 'react';
+import {signOut} from 'firebase/auth'
+import {auth} from "./firebase-config"
+
 
 function App() {
+  const [isAuth,setisAuth] = useState(false);
+
+  const signUserOut=()=>{
+    signOut(auth).then(()=>{
+      localStorage.clear()
+      setisAuth(false)
+      window.location.pathname='/login'
+    })
+  }
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/create">CreatePost</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/createpost">CreatePost</Link>
+      {!isAuth?  <Link to="/login">Login</Link>:<button onClick={signUserOut}>Logout</button>}
 
       </nav>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/createpost" element={<CreatePost/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>} />
+        <Route path="/login" element={<Login setisAuth={setisAuth}/>} />
       </Routes>
     </Router>
   );
--- blog-app/src/pages/Login.js
+++ blog-app/src/pages/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {auth,provider} from "../firebase-config"
+import { signInWithPopup} from 'firebase/auth'
+import {  useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({setisAuth}) => {
+    let navigate=useNavigate();
+
+  const signInWithGoogle=()=>{
+    signInWithPopup(auth,provider).then((result)=>{
+     localStorage.setItem("isAuth",true);
+     setisAuth(true)
+     navigate("/")
+    })
+  }
+
   return (
-    <div>
-      Login
+    <div className='loginPage'>
+      <p>Sign in With Google</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>Signin With Google</button>
     </div>
   )
 }
